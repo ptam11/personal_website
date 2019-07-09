8d92fea8530a9b73fb11424bd9562b11976d6539
Parco Tam <ptam1556@gmail.com>
2019-07-09 10:43:39 -0700

    added resume google drive link

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -14,7 +14,7 @@ class Footer extends Component {
 				<a className="nav-link" href="https://github.com/ptam11">
 					<FontAwesomeIcon className='Footer-link Footer-github' icon={faGithub} /> 
 				</a>
-				<a className="nav-link" href="/public/parco_tam.pdf" download>
+				<a className="nav-link" href="https://drive.google.com/file/d/1A8dEHOPYnCbmcXM9XE-O5AGQ3w5X8ifn/view?usp=sharing" >
 					<FontAwesomeIcon className='Footer-link Footer-resume' icon={faFile} /> 
 				</a>
 				<a className="nav-link" href="https://github.com/ptam11">
