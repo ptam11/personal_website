9cfdb44b21e032598e42066bc9458a48b54c3645
Parco Tam <ptam1556@gmail.com>
2019-08-11 00:42:27 -0700

    updated about me

--- src/components/specific/aboutInfo/aboutInfo.js
+++ src/components/specific/aboutInfo/aboutInfo.js
@@ -7,7 +7,7 @@ const desListArr =[
 	{title: "GOAL:", des: "Work for a company that improves the way we live intelligently through technology."},
 	{title: "DATA ANALYST @BLOOMBERG:", des: "I loved my job at Bloomberg. Through the 7 years, I applied what I learnt from school, worked with product managers, R&D and sales to improve the product and defended essential financial data that impacted millions of clients."},
 	{title: "LESSON LEARNED:", des: "Data wasn't about manually punching in numbers. I left the company with fascination over the massive database architecture and network of data flows behind the scenes."},
-	{title: "WHY I LEFT MY JOB:", des: "I was inspired after taking my data-science course. It taught me how to approach problems logically and methodologically. It was a great feeling to use tech to simplify a large problem to solve! I wanted to take a break and fully invest my time in learning technical skills to transition my career into an software engineer."},
+	{title: "WHY I LEFT MY JOB:", des: "I was inspired after taking my data-science course. It taught me how to approach problems logically and methodologically. It was a great feeling to use tech to simplify a large problem to solve! I decided to take a break to fully invest my time to pivot my career into a more technical role."},
 ];
 
 
